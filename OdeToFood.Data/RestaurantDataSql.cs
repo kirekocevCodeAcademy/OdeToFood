31bd25b42bf7573a94cc74986873c16bc9f61d80
kirekocevCodeAcademy <kirekocevCodeAcademy@gmail.com>
2020-02-17 19:26:30 +0100

    SQL implementation.

---
+++ OdeToFood.Data/RestaurantDataSql.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public class RestaurantDataSql : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext odeToFoodDbContext;
+
+        public RestaurantDataSql(OdeToFoodDbContext odeToFoodDbContext)
+        {
+            this.odeToFoodDbContext = odeToFoodDbContext;
+        }
+
+        public int Commit()
+        {
+            return odeToFoodDbContext.SaveChanges();
+        }
+
+        public Restaurant Create(Restaurant restaurant)
+        {
+            odeToFoodDbContext.Restaurants.Add(restaurant);
+            return restaurant;
+        }
+
+        public Restaurant GetRestaurantById(int restaurantId)
+        {
+            return odeToFoodDbContext.Restaurants.SingleOrDefault(r => r.Id == restaurantId);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurants(string name = null)
+        {
+            var param = !string.IsNullOrEmpty(name) ? $"{name}%" : name;
+            return odeToFoodDbContext.Restaurants.Where(r => string.IsNullOrEmpty(name) || EF.Functions.Like(r.Name, param)).ToList();
+        }
+
+        public Restaurant Update(Restaurant restaurant)
+        {
+            odeToFoodDbContext.Entry(restaurant).State = EntityState.Modified;
+            return restaurant;
+        }
+    }
+}
