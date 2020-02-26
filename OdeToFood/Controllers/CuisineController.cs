d4d154ce3e4e0ef1ed9cad46b194c4a8ea5d05b1
kirekocevCodeAcademy <kirekocevCodeAcademy@gmail.com>
2020-02-26 20:59:11 +0100

    API

---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Controllers
+{
+    [Route("api/Restaurants")]
+    [ApiController]
+    public class CuisineController : ControllerBase
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public CuisineController(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        [HttpGet("cusine")]
+        public IActionResult GetAll()
+        {
+            return Ok(Enum.GetNames(typeof(CuisineType)));
+        }
+
+        [HttpGet("{resturantId}/cusine")]
+        public IActionResult GetByRestuarntId(int resturantId)
+        {
+            var resturant = restaurantData.GetRestaurantById(resturantId);
+            if(resturant == null)
+            {
+                return NotFound();
+            }
+            return Ok(resturant.Cuisine.ToString());
+        }
+    }
+}
---
+++ OdeToFood/Controllers/RestaurantApiController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Data;
+
+namespace OdeToFood.Controllers
+{
+    [Route("api/Restaurants")]
+    [ApiController]
+    public class RestaurantApiController : ControllerBase
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public RestaurantApiController(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+               
+        [HttpGet]
+        public IActionResult GetRestaurantsAll()
+        {
+            var data = restaurantData.GetRestaurants();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetRestaurant(int id)
+        {
+            var data = restaurantData.GetRestaurantById(id);
+            if(data == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(data);
+        }
+    }
+}
